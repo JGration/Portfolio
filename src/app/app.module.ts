bb22157c8019e75cf905708d7d7ab01ec784100c
jordanhfg <jordan.goncalves@herodigital.com>
2022-06-21 15:22:42 -0300

    Revert "reset"
    This reverts commit 9c14624aacc32067b5efc0e9b208cedb6650d40e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavComponent } from './nav/nav.component';
-import { FooterComponent } from './footer/footer.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { ScrollComponent } from './animations/scroll/scroll.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    NavComponent,
-    FooterComponent,
-    ExperienceComponent,
-    ScrollComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
