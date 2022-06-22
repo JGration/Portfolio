a4ff14d72455dd4f61e97a6bc4e390989c52fa87
jordanhfg <jordan.goncalves@herodigital.com>
2022-06-22 01:19:19 -0300

    changed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 import { Animations } from './animations'
-var mobile = require('is-mobile');
+var mobile = require('is-mobile')
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: Animations.animate
-  
 })
 export class AppComponent {
-  title = 'portfoliobackup';
+  title = 'portfolio'
   state = 'inactive'
   scroll = 'hide'
-  
+
   ngOnInit (): void {
-    
     setTimeout(() => {
       this.state = 'active'
     }, 300)
-    if(mobile()){
+    if (mobile()) {
       this.scroll = 'show'
     }
   }
-
 }
