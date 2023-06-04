86f4540180811df13e3fcae1159a12ba47b0ca97
jordanhfg <jordan.goncalves@herodigital.com>
2023-06-04 16:08:15 -0300

    after watching a BR video

--- src/app/animations/animations.animation.ts
+++ src/app/animations/animations.animation.ts
@@ -4,69 +4,68 @@ import {
   state,
   style,
   transition,
-  trigger
-} from '@angular/animations'
+  trigger,
+} from '@angular/animations';
 
 const rightStartSlide = style({
   transform: 'translate(100%, 0)',
-  opacity: 0
-})
+  opacity: 0,
+});
 const rightEndSlide = style({
   transform: 'translate(0, 0)',
-  opacity: 1
-})
+  opacity: 1,
+});
 const hidden = style({
-  opacity: 0
-})
+  opacity: 0,
+});
 const visible = style({
-  opacity: 1
-})
+  opacity: 1,
+});
 const bottomStartSlide = style({
   opacity: 0,
-  transform: 'translate(0, 100%)'
-})
+  transform: 'translate(0, 100%)',
+});
 const bottomEndSlide = style({
   opacity: 1,
-  transform: 'translate(0)'
-})
+  transform: 'translate(0)',
+});
 const hiddenScroll = style({
-  opacity: 0
-})
+  opacity: 0,
+});
 const visibleScroll = style({
-  opacity: 1
-})
+  opacity: 1,
+});
 
 export class Animations {
-  static animate = Animations.getAnimations()
+  static animate = Animations.getAnimations();
 
-  static getAnimations (): Array<AnimationTriggerMetadata> {
+  static getAnimations(): Array<AnimationTriggerMetadata> {
     return [
       trigger('fadeState', [
         state('inactive', hidden),
         state('active', visible),
-        transition('inactive => active', animate('1.5s ease-in-out'))
+        transition('inactive => active', animate('0.8s ease-in-out')),
       ]),
       trigger('bottomSlideState', [
         state('inactive', bottomStartSlide),
         state('active', bottomEndSlide),
-        transition('inactive => active', animate('1.5s ease-in-out'))
+        transition('inactive => active', animate('1s ease-in-out')),
       ]),
       trigger('rightSlideState', [
         state('inactive', rightStartSlide),
         state('active', rightEndSlide),
-        transition('inactive => active', animate('1s ease-in-out'))
+        transition('inactive => active', animate('1s ease-in-out')),
       ]),
       trigger('scrollSlide', [
         state('hide', rightStartSlide),
         state('show', rightEndSlide),
-        transition('hide => show', animate('1400ms ease-out'))
+        transition('hide => show', animate('1000ms ease-out')),
       ]),
       trigger('scroll', [
         state('hide', hiddenScroll),
         state('show', visibleScroll),
-        transition('hide => show', animate('3s ease-in-out'))
-      ])
-
-    ]
+        transition('hide => show', animate('1.5s ease-in-out')),
+      ]),
+    ];
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,47 +1,46 @@
-import { animate } from '@angular/animations'
-import { Component, OnInit, AfterViewInit } from '@angular/core'
-import { Animations } from '../animations'
-import anime from 'animejs'
+import { Component, AfterViewInit } from '@angular/core';
+import { Animations } from '../animations';
+import anime from 'animejs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: Animations.animate
+  animations: Animations.animate,
 })
 export class HomeComponent implements AfterViewInit {
-  state = 'inactive'
+  state = 'inactive';
 
-  constructor () {}
+  constructor() {}
 
-  ngOnInit (): void {
-    
+  ngOnInit(): void {
     setTimeout(() => {
-      this.state = 'active'
-    }, 4500)
+      this.state = 'active';
+    }, 4000);
   }
 
-  ngAfterViewInit () {
-    const element = document.querySelector<HTMLElement>('.text-animation')
+  ngAfterViewInit() {
+    const element = document.querySelector<HTMLElement>('.text-animation');
     var lettersHtml =
       element?.textContent?.replace(/\S/g, '<span class="letter">$&</span>') ||
-      ''
-    lettersHtml = lettersHtml.replace('<span class="letter">D</span>',
+      '';
+    lettersHtml = lettersHtml.replace(
+      '<span class="letter">D</span>',
       '<span class="letter">&nbsp</span><span class="letter">D</span>'
-    )
-    element!.innerHTML = `<div class="letters">${lettersHtml}</div><span class="cursor"></span>`
-    element!.style.display = 'block'
+    );
+    element!.innerHTML = `<div class="letters">${lettersHtml}</div><span class="cursor"></span>`;
+    element!.style.display = 'block';
 
-    const letters = Array.from(element!.querySelectorAll('.letter'))
-    const TYPE_AFTER_MS = 1800
-    const JUMP_AFTER_MS = 150
+    const letters = Array.from(element!.querySelectorAll('.letter'));
+    const TYPE_AFTER_MS = 1000;
+    const JUMP_AFTER_MS = 100;
 
     const blink = anime({
       targets: '.text-animation .cursor',
       loop: true,
       duration: 750,
-      opacity: [{ value: [1, 1] }, { value: [0, 0] }]
-    })
+      opacity: [{ value: [1, 1] }, { value: [0, 0] }],
+    });
 
     Promise.resolve(
       anime
@@ -54,8 +53,8 @@ export class HomeComponent implements AfterViewInit {
             translateX: letters.map((letter: any, i) => ({
               value: letter.offsetLeft + letter.offsetWidth,
               duration: 1,
-              delay: i === 0 ? 0 : JUMP_AFTER_MS
-            }))
+              delay: i === 0 ? 0 : JUMP_AFTER_MS,
+            })),
           },
           TYPE_AFTER_MS
         )
@@ -66,22 +65,19 @@ export class HomeComponent implements AfterViewInit {
             duration: 1,
             delay: anime.stagger(JUMP_AFTER_MS),
             changeBegin: () => {
-              blink.pause()
+              blink.pause();
             },
             changeComplete: () => {
-              blink.restart()
-            }
+              blink.restart();
+            },
           },
           TYPE_AFTER_MS
         )
-    ).then(function resolve () {
-
-      
-
-      setTimeout(function timeout () {
-        var cursor = document.querySelector<HTMLElement>('.cursor')
-        cursor?.parentNode?.removeChild(cursor)
-      }, 8000)
-    })
+    ).then(function resolve() {
+      setTimeout(function timeout() {
+        var cursor = document.querySelector<HTMLElement>('.cursor');
+        cursor?.parentNode?.removeChild(cursor);
+      }, 4000);
+    });
   }
 }
